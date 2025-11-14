29983519a41e86734b7960fdaecf4f5bd98219b8
Stefan Riegel <stefan@riegel.me>
2025-11-14 18:38:21 +0100

    Fix Terraform syntax error in record_generic module

--- modules/record_generic/main.tf
+++ modules/record_generic/main.tf
@@ -1,8 +1,10 @@
 locals {
   # Build the rdata object based on the record type.
-  rdata = var.record_type == "A"    ? { address  = var.record_value } :
-          var.record_type == "AAAA" ? { address6 = var.record_value } :
-                                      { text     = var.record_value }
+  rdata = (
+    var.record_type == "A" ? { address = var.record_value } :
+    var.record_type == "AAAA" ? { address6 = var.record_value } :
+    { text = var.record_value }
+  )
 }
 
 resource "bloxone_dns_record" "this" {
